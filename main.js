547924afb30ed088397f32c3c991fe0fb30b1198
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2021-08-24 21:35:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
+var mouse_event = "empty";
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
+color = "black";
+width_of_line = 1;
+radius = 20;
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
 color = document.getElementById("color").value;
+width_of_line = document.getElementById("width_of_line").value;
 radius = document.getElementById("radius").value;
-ctx.lineWidth = document.getElementById("width_of_line").value;
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.arc( 200,200,radius,0,2*Math.PI);
+mouse_event = "mousedown";
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+function my_mousemove(e){
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    if(mouse_event == "mousedown"){
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius,0,2*Math.PI);
 ctx.stroke();
-canvas.addEventListener("mousedown",my_mousedown);
-function my_mousedown(e) {
-    color = document.getElementById("color").value;
-console.log(color);
-mouse_x = e.clientX - canvas.offsetLeft;
-mouse_y = e.clientY - canvas.offsetTop;
-radius = document.getElementById("radius").value;
-console.log("X = " + mouse_x + " ,Y = " + mouse_y);
-console.log("Radius is " + radius);
-circle(mouse_x, mouse_y);
+    }
 }
-function circle(mouse_x, mouse_y){
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = document.getElementById("width_of_line").value;
-    ctx.arc( mouse_x,mouse_y,radius,0,2*Math.PI);
-    ctx.stroke();
+
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e){
+    mouse_event = "mouseup";
 }
-function clear_area() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e){
+    mouse_event = "mouseleave";
+}
+
+function clearArea(){
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 }
